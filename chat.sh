b06ed86f5888362702857d6f3fc5c565d024e60f
Jan Krag <jankrag@gmail.com>
2021-02-09 15:02:26 +0100

    Small improvements to script
    Mainly as suggested by ShellCheck

--- chat.sh
+++ chat.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
+
 CYAN='\033[0;36m'
 GREEN='\033[0;32m'
 RED='\033[0;31m'
 RC='\033[0m' # Reset Color
+
 echo -e "${CYAN}Welcome to GitSlick, the Git based miniature chat app.${RC}"
 echo
 echo "Commands:"
@@ -19,16 +21,17 @@ echo
 echo -e "You are currenty in folder ${RED}$(pwd)${RC}"
 echo -e "And pushing to ${RED}$(git remote get-url origin --push)${RC}"
 echo
+
 git fetch --quiet
-git log --pretty="format:%C(green)%aN %C(blue)[%ad]%C(reset)%n%B" --date=relative ..@{u}
+git log --pretty="format:%C(green)%aN %C(blue)[%ad]%C(reset)%n%B" --date=relative "..@{u}"
 git merge --ff-only --quiet
 while true ; do
   while IFS=" " read -r -e -p "$(git branch --show-current)> " cm options; do
     #echo "[$cm] [$options]"
     git fetch --quiet
-    git log --pretty="format:%C(green)%aN %C(blue)[%ad]%C(reset)%n%B" --date=relative ..@{u}
+    git log --pretty="format:%C(green)%aN %C(blue)[%ad]%C(reset)%n%B" --date=relative "..@{u}"
     git merge --ff-only --quiet
-    echo 
+    echo
     if [ "$cm" = "" ]; then
       :
     elif [ "$cm" = "/quit" ]; then
@@ -44,15 +47,15 @@ while true ; do
       echo "Creating new channel $options"
       git switch --quiet --orphan "$options"
       git commit --allow-empty --message "Beginning of Channel $options"
-      git push --set-upstream origin "$options"
+      git push --set-upstream --quiet origin "$options"
     elif [ "$cm" = "/repeat" ]; then
-      : "${options:=5}" 
+      : "${options:=5}"
       echo -e "Repeating last $options messages in ${RED}$(git branch --show-current)${RC}"
       git log "-$options" --pretty="format:%C(green)%aN %C(blue)[%ad]%C(reset)%n%B" --date=relative
     else
       message="$cm $options"
       git commit --quiet --allow-empty --message "$message"
-      git log -1 --pretty="format:%C(green)%aN %C(blue)[%ad]%C(reset)%n%B" --date=relative 
+      git log -1 --pretty="format:%C(green)%aN %C(blue)[%ad]%C(reset)%n%B" --date=relative
       git push --quiet origin
     fi
   done
